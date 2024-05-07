30dcf1f44028507883cb142e92e19a1ac1a47591
Shiro995 <72838778+Shiro995@users.noreply.github.com>
2024-05-07 14:41:24 +0300

    life is working

--- JavaScript/ProjectLibrary/script.js
+++ JavaScript/ProjectLibrary/script.js
@@ -3,31 +3,93 @@ const body = document.querySelector("body")
 
 const book = {
     title: "text",
-    author: "text",
     pages: "number",
+    author: "text",
     isRead: "checkbox",
     // info = (description) => {return description;}
     //image
 };
-
-const MyLibrary = [];
+let MyLibrary = [];
 let currentBook = [];
 let v = 1;
+let EventListeners = [];
+let defaultBook = ["The Default mysteries of Default", "420", "Mo Ke", false]
 
-function addBookToLibrary(book) {
-    let container = document.createElement("div")
-    container.setAttribute("style", "border: 2px solid black; border-radius: 10px; padding: 20px; display: flex; flex-wrap: wrap; width: 20%; height: 40%")
+addBookToLibrary(defaultBook)
 
-    let title = document.createElement("h3")
-    title.textContent = book[0];
 
-    container.appendChild(title)
-    body.appendChild(container)
-}
 
 document.querySelector('.addbook').addEventListener("click", () => {
     addForm()
 })
+
+function createCheckboxEventListener(id) {
+    const isRead = document.querySelector(`#isRead${id}`)
+
+
+    isRead.addEventListener('change', () => {
+        if(isRead.checked) {
+            MyLibrary[id-1][3] = true
+        } else {
+            MyLibrary[id-1][3] = false
+        }
+
+    })
+
+    // EventListeners.push()
+
+}
+function addBookToLibrary(Ebook) {
+    let container = document.createElement("div")
+    container.setAttribute("class", "book")
+    
+    // let title = document.createElement("p")
+    // title.classList.add("title")
+    // title.textContent = book[0];
+
+    // let author = document.createElement("p")
+    // author.textContent = book[1]
+    let i = 0
+    for (const key in book) {
+
+        let element = document.createElement("p")
+        element.setAttribute("class", `${key}`)
+        element.textContent = Ebook[i]
+        i++
+        if (key === 'pages' && Ebook[1] !== '')
+        element.textContent += " pages"   
+        if (key === 'isRead')
+        transformCheck(element, Ebook[3])
+
+        container.appendChild(element)
+    }
+
+    // let cover = document.createElement("div")
+    // cover.setAttribute("class", "cover")
+    // container.appendChild(cover)
+    //cover image goes here
+    body.appendChild(container)
+    
+    MyLibrary[v-1] = [...Ebook]
+    EventListeners.push(createCheckboxEventListener(v))
+}
+
+function transformCheck(element, bool) {
+    // element.remove()
+    let label = document.createElement("label")
+    let checkbox = document.createElement("input")
+    checkbox.setAttribute("type", "checkbox")
+    checkbox.setAttribute("id", `isRead${v}`)
+    label.textContent = "Read?"
+
+    label.appendChild(checkbox)
+    // element.remove()
+    element.textContent=""
+    element.appendChild(label)
+    if(bool === true)
+    checkbox.setAttribute("checked", "")
+
+}
 
 function addForm() {
     const dialog = document.createElement("dialog")
@@ -66,8 +128,7 @@ function addForm() {
             }
             i++
         }
-        MyLibrary.push(currentBook)
-        dialog.close()
+        dialog.remove()
 
         addBookToLibrary(currentBook)
     })
